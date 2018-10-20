de94dd1a7bef08c7549d5b229920c47550fd9bd8
Yoonki Park <yoonki.park@samsung.com>
2018-10-20 18:40:01 +0900

    project init
    Signed-off-by: Yoonki Park <yoonkiss.park@gmail.com>

---
+++ src/main/java/com/example/moneybook/moneybook/MoneybookApplication.java
@@ -0,0 +1,12 @@
+package com.example.moneybook.moneybook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneybookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneybookApplication.class, args);
+	}
+}
